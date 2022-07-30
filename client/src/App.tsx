40a761878f68afd8d6b8c4232161a8664b5cf561
Filipe Andrade <filipe.engenharia42@gmail.com>
2022-07-30 19:42:06 -0300

    added todo mark as done when clicking the check icon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
-import { Box } from "@mantine/core";
+import { Box, List, ThemeIcon } from "@mantine/core";
+import { CheckCircleFillIcon } from "@primer/octicons-react";
 import useSWR from "swr";
 import './App.css'
 import './components/AddTodo'
@@ -18,9 +19,44 @@ const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((r) => r.json(
 function App() {
   const {data, mutate} = useSWR<Todo[]>("api/todos", fetcher)
 
-  return (
-    <Box >{JSON.stringify(data)}
+  async function markTodoAsDone(id: number) {
+    const updated = await fetch(`${ENDPOINT}/api/todos/${id}/done`, {
+      method: "PATCH",
+    }).then((r) => r.json());
 
+    mutate(updated);
+  }
+
+  return (
+    <Box
+    sx={(theme) => ({
+      padding: "2rem",
+      width: "100%",
+      maxWidth: "40rem",
+      margin: "0 auto",
+    })}
+    >
+      <List spacing="xs" size="sm" mb={12} center>
+        {data?.map((todo) => {
+          return (
+            <List.Item
+            onClick={ () => markTodoAsDone(todo.id) }
+            key = {`todo_list__${todo.id}`}
+
+            icon={
+              todo.done ? (
+              <ThemeIcon color="teal" size={24} radius="xl">
+                <CheckCircleFillIcon size={20} />
+              </ThemeIcon>) : (
+              <ThemeIcon color="gray" size={24} radius="xl">
+                <CheckCircleFillIcon size={20} />
+              </ThemeIcon>)
+            }
+
+            >{todo.title}</List.Item>
+          )
+        })}
+      </List>
     <AddTodo mutate={mutate}></AddTodo>
     </Box >
   )
