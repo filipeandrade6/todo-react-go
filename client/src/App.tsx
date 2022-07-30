639fe34775426673a1b7317ba3964d2b9380c60e
Filipe Andrade <filipe.engenharia42@gmail.com>
2022-07-30 19:27:59 -0300

    adding logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,28 @@
 import { Box } from "@mantine/core";
 import useSWR from "swr";
 import './App.css'
+import './components/AddTodo'
+import AddTodo from "./components/AddTodo";
 
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+}
 
 export const ENDPOINT = "http://localhost:4000";
 
 const fetcher = (url: string) => fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
 
 function App() {
-  const {data, mutate} = useSWR("api/todos", fetcher)
+  const {data, mutate} = useSWR<Todo[]>("api/todos", fetcher)
 
   return (
-    <Box >{JSON.stringify(data)}</Box >
+    <Box >{JSON.stringify(data)}
+
+    <AddTodo mutate={mutate}></AddTodo>
+    </Box >
   )
 }
 
